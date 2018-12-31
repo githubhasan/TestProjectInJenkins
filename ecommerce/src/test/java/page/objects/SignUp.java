b37063dce6d06c4efbaf93e35622c4de6330b5f0
Md Showkat Hasan <45223299+githubhasan@users.noreply.github.com>
2018-12-31 03:08:41 -0500

    test13
    test13

--- ecommerce/src/test/java/page/objects/SignUp.java
+++ ecommerce/src/test/java/page/objects/SignUp.java
@@ -10,16 +10,16 @@ public class SignUp extends BrowserDriver {
         driver.findElement(By.partialLinkText("Create Account")).click();
 
         driver.findElement(By.id("FirstNameBilling")).sendKeys("Md");
-        Thread.sleep(2000);
+        Thread.sleep(1000);
         driver.findElement(By.id("LastNameBilling")).sendKeys("Hasan");
-        Thread.sleep(2000);
+        Thread.sleep(1000);
 
         driver.findElement(By.id("Username2")).sendKeys("vala_nai@yahoo.com");
-        Thread.sleep(2000);
+        Thread.sleep(1000);
         driver.findElement(By.id("Password1")).sendKeys("Test@1234");
-        Thread.sleep(2000);
+        Thread.sleep(1000);
         driver.findElement(By.id("Password2")).sendKeys("Test@1234");
-        Thread.sleep(2000);
+        Thread.sleep(1000);
 
         driver.findElement(By.className("ccBtnSubmit")).click();
 
